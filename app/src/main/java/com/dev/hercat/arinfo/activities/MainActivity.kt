0fba0ab61c8898b97fa6c378986c7f57b95b459c
hercat <991146900@qq.com>
2019-01-02 10:41:53 +0800

    add geo fence support

--- app/src/main/java/com/dev/hercat/arinfo/activities/MainActivity.kt
+++ app/src/main/java/com/dev/hercat/arinfo/activities/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.dev.hercat.arinfo.activities
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.Color
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.ListView
-import com.amap.api.location.AMapLocation
-import com.amap.api.location.AMapLocationClient
-import com.amap.api.location.AMapLocationClientOption
-import com.amap.api.location.AMapLocationListener
+import com.amap.api.fence.GeoFence
+import com.amap.api.fence.GeoFenceClient
+import com.amap.api.location.*
 import com.amap.api.maps.AMap
 import com.amap.api.maps.model.LatLng
 import com.amap.api.maps.model.MyLocationStyle
@@ -29,6 +33,8 @@ import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
     val TAG = "ACTIVITY_MAIN"
+
+    private val GEO_FENCE_ACTION = "con.dev.hercat.arinfo.broadcast"
 
     private val points = listOf(
             Point(106.553696,29.519781, "怡丰花园", "桃源路怡丰花园", R.drawable.yfhy),
@@ -55,6 +61,9 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         drawArea()
     }
 
+    /**
+     * start location client
+     */
     private fun initLocationClient() {
         if (!checkPermission()) {
             getPermission(object : PermissionCallback {
@@ -68,7 +77,7 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         mLocationClient.setLocationOption(locationClientConfiguratiion)
 
         mLocationClient.setLocationListener {aMapLocation ->
-            if (aMapLocation is AMapLocation) {
+           if (aMapLocation is AMapLocation) {
                 if (aMapLocation.errorCode == 0) {
                     //get location success
                     tvLocation.text = resources.getString(R.string.location_desc,
@@ -88,6 +97,9 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         mLocationClient.startLocation()
     }
 
+    /**
+     * init orientation sensor
+     */
     private fun initSensor() {
         val rotationCache = mutableListOf<Float>()
         Sensey.getInstance().startRotationAngleDetection { x: Float, _: Float, _: Float ->
@@ -105,6 +117,9 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
 
     }
 
+    /**
+     * init map layer
+     */
     private fun initMap() {
         val mLocationStyle = MyLocationStyle()
         // set fetch location interval
@@ -114,6 +129,10 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         map.myLocationStyle = mLocationStyle
         map.isMyLocationEnabled = true
     }
+
+    /**
+     * draw target argument reality area
+     */
     private fun drawArea() {
         val latlngs = arrayOf(LatLng(29.519142, 106.553157),
                 LatLng(29.518722,106.554734), LatLng(29.520076,106.554702),
@@ -124,6 +143,47 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
                 .strokeColor(Color.argb(50, 1, 1,1))
                 .fillColor(Color.argb(50, 0, 0,255))
         map.addPolygon(polygonOptions)
+    }
+
+
+    /**
+     * draw target argument reality fence
+     */
+    private fun createFence() {
+        val mGeoFenceClient = GeoFenceClient(applicationContext)
+        mGeoFenceClient.setActivateAction(GeoFenceClient.GEOFENCE_IN or GeoFenceClient.GEOFENCE_OUT)
+        val points = listOf(DPoint(29.519142, 106.553157), DPoint(29.518722,106.554734),
+                DPoint(29.520076,106.554702), DPoint(29.520818,106.553237), DPoint(29.520505,106.552352))
+        // create fence
+        mGeoFenceClient.addGeoFence(points, "YFHY001")
+        // add fence create result callback
+        mGeoFenceClient.setGeoFenceListener { _, errorCode, _ ->
+            if (errorCode == GeoFence.ADDGEOFENCE_SUCCESS) {
+                Log.i(TAG, "add fence success")
+            } else {
+                Log.i(TAG, "add fence failed")
+            }
+        }
+        // create PendingIntent
+        mGeoFenceClient.createPendingIntent(GEO_FENCE_ACTION)
+
+        val fenceBroadCastReceiver = object: BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                if (intent.action == GEO_FENCE_ACTION) {
+                    // parse result
+                    val bundle = intent.extras
+                    val status = bundle.getInt(GeoFence.BUNDLE_KEY_FENCESTATUS)
+                    val fenceId = bundle.getString(GeoFence.BUNDLE_KEY_FENCEID)
+                    val customId = bundle.getString(GeoFence.BUNDLE_KEY_CUSTOMID)
+                    // the trigger fence
+                    val fence = bundle.getParcelable(GeoFence.BUNDLE_KEY_FENCE) as GeoFence
+                }
+            }
+        }
+        val intentFilter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+        intentFilter.addAction(GEO_FENCE_ACTION)
+        // register broadcast receiver
+        registerReceiver(fenceBroadCastReceiver, intentFilter)
     }
 
     override fun onResume() {
